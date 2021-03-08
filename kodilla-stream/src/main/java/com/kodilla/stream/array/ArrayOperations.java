5b094b058b4aab56f8a57f22fefcb54f36269415
Kamil <kamil.krol@gmail.com>
2021-03-09 00:29:17 +0100

    Zadanie 7.5 sposób bez range

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -6,12 +6,22 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
     public static double getAverage(int[] numbers)
     {
-        IntStream streamFromArray = Arrays.stream(numbers);
-         IntStream.range(0, numbers.length)
-                .forEach(n -> System.out.println(n));
-         return 7.7;
+       // IntStream streamFromArray = Arrays.stream(numbers);
+//        IntStream.range(0, 20)
+//                .
+//                .forEach();
+//         return 7.7;
 //         return IntStream.range(0, numbers.length)
 //                 .forEach(System.out::println);
+        IntStream.of(numbers)
+        .forEach(n-> System.out.println(n));
+
+        return IntStream.of(numbers)
+                .average().getAsDouble();
+
+
+
+
 
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -15,12 +15,12 @@ public class ArrayOperationsTestSuite {
     void testGetAverage()
     {
         //Given
-        int[] tab = {3,4,6,7,5,3,2,6,9,0,-2,-5,3,4,6,8,2,3,6,7};
+        int[] tab = {2,4,6,8,4,3,5,6,7,9,-2,-4};
 
         //When
         double result = getAverage(tab);
 
         //Then
-        assertEquals(7.7, result);
+        assertEquals(4, result);
     }
 }
