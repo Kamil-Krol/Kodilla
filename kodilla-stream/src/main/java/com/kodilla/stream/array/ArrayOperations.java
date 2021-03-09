a6302283bf3c57558fa5acf9b5be72a560a75e27
Kamil <kamil.krol@gmail.com>
2021-03-09 17:30:42 +0100

    Zadanie 7.5 v1

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,23 +1,26 @@
 package com.kodilla.stream.array;
 
-import java.util.Arrays;
+
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     public static double getAverage(int[] numbers)
     {
-       // IntStream streamFromArray = Arrays.stream(numbers);
-//        IntStream.range(0, 20)
-//                .
-//                .forEach();
-//         return 7.7;
-//         return IntStream.range(0, numbers.length)
-//                 .forEach(System.out::println);
-        IntStream.of(numbers)
+
+        IntStream.range(0, numbers.length)
+                .map(index->numbers[index])
+                .forEach(s-> System.out.println(s));
+
+         double result = IntStream.range(0, numbers.length)
+                 .map(index->numbers[index])
+                 .average().getAsDouble();
+         return result;
+
+        /*IntStream.of(numbers)
         .forEach(n-> System.out.println(n));
 
         return IntStream.of(numbers)
-                .average().getAsDouble();
+                .average().getAsDouble();*/
 
 
 
