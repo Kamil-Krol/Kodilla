68d1a3cbe1169165cc275b3275c48ea799873c24
Kamil <kamil.krol@gmail.com>
2021-07-15 18:27:45 +0200

    Zadanie6.7 niedokończone

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumCalculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumCalculator {
+
+    public  void calculateAdvStatistics(Statistics statistics)
+    {
+
+    }
+
+    public void showStatistics()
+    {
+
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/TestForumCalculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.jupiter.api.Test;
+
+public class TestForumCalculator {
+
+    @Test
+    void testcalculateAdvStatistics()
+    {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+}
