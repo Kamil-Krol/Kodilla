c52ce00daf0ae745fef53efa4b8bd811c8e1b03a
Kamil <kamil.krol@gmail.com>
2021-08-07 21:29:49 +0200

    Zadanie14.3v1

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.portfolio;
+
+import com.kodilla.spring.reader.ReaderConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    public void testTaskAdd()
+    {
+        //Given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = applicationContext.getBean(Board.class);
+        //When
+        boolean result = applicationContext.containsBean("board");
+        //Then
+        System.out.println("Bean found in container: " + result);
+    }
+}
