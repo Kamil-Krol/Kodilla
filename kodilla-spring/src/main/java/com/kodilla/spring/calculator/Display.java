424fbd6eb1b504cd855216050bfd90a5decfb4c8
Kamil <kamil.krol@gmail.com>
2021-08-07 17:07:51 +0200

    zadanie14.2v1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public final class Display {
+
+    public void displayValue( double val)
+    {
+        System.out.println(val);
+    }
+}
