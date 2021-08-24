07e9b1af38b21ad691f300ab2abae6ad85f7baa9
Kamil <kamil.krol@gmail.com>
2021-08-24 12:39:53 +0200

    Zadanie 16.6 v1

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -5,28 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class DbManager {
-    private Connection conn;                                          // [1]
-    private static DbManager dbManagerInstance;                       // [2]
+public enum DbManager {
 
-    private DbManager() throws SQLException {                         // [3]
-        Properties connectionProps = new Properties();                 // [4]
-        connectionProps.put("user", "kodilla_user");                   // [5]
-        connectionProps.put("password", "kodilla_Pass123");           // [6]
-        conn = DriverManager.getConnection(                            // [7]
-                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +  // [8]
-                        "&useSSL=False",                                             // [9]
-                connectionProps);                                              // [10]
-    }                                                                 // [11]
+    INSTANCE;                                                  // [1]
 
-    public static DbManager getInstance() throws SQLException {       // [12]
-        if (dbManagerInstance == null) {                               // [13]
-            dbManagerInstance = new DbManager();                        // [14]
-        }                                                              // [15]
-        return dbManagerInstance;                                      // [16]
-    }                                                                 // [17]
+    private Connection conn;                                   // [2]
 
-    public Connection getConnection() {                               // [18]
-        return conn;                                                   // [19]
-    }
+    DbManager() {                                              // [3]
+        Properties connectionProps = new Properties();          // [4]
+        connectionProps.put("user", "kodilla_user");            // [5]
+        connectionProps.put("password", "kodilla_Pass123");     // [6]
+        try {
+            conn = DriverManager.getConnection(                  // [7]
+                    "jdbc:mysql://localhost:3306/kodilla_course" +
+                            "?serverTimezone=Europe/Warsaw" +
+                            "&useSSL=False",                                  // [10]
+                    connectionProps);                                 // [11]
+        } catch (SQLException e) {                              // [12]
+            throw new ExceptionInInitializerError(e);            // [13]
+        }                                                       // [14]
+    }                                                          // [15]
+
+    public static DbManager getInstance() {                    // [16]
+        return INSTANCE;                                        // [17]
+    }                                                          // [18]
+
+    public Connection getConnection() {                        // [19]
+        return conn;                                            // [20]
+    }                                                          // [21]
 }
