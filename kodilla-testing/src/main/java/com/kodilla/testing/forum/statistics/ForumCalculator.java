bafd0e2ebad86c2252ddf76edb3e0f8732ddfe3f
Kamil <kamil.krol@gmail.com>
2021-09-21 23:10:16 +0200

    Zadanie 6.7 niedokonczone

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumCalculator.java
@@ -2,6 +2,19 @@ package com.kodilla.testing.forum.statistics;
 
 public class ForumCalculator {
 
+
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private double averageUserPosts;
+    private double averageUserComments;
+    private double averagePostComments;
+    Statistics statistics;
+
+    public ForumCalculator(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
     public  void calculateAdvStatistics(Statistics statistics)
     {
 
@@ -10,5 +23,53 @@ public class ForumCalculator {
     public void showStatistics()
     {
 
+    }
+
+    public int getNumberOfUsers() {
+        return numberOfUsers;
+    }
+
+    public int getNumberOfPosts() {
+        return numberOfPosts;
+    }
+
+    public int getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public double getAverageUserPosts() {
+        return averageUserPosts;
+    }
+
+    public double getAverageUserComments() {
+        return averageUserComments;
+    }
+
+    public double getAveragePostComments() {
+        return averagePostComments;
+    }
+
+    public void setNumberOfUsers(int numberOfUsers) {
+        this.numberOfUsers = numberOfUsers;
+    }
+
+    public void setNumberOfPosts(int numberOfPosts) {
+        this.numberOfPosts = numberOfPosts;
+    }
+
+    public void setNumberOfComments(int numberOfComments) {
+        this.numberOfComments = numberOfComments;
+    }
+
+    public void setAverageUserPosts(double averageUserPosts) {
+        this.averageUserPosts = averageUserPosts;
+    }
+
+    public void setAverageUserComments(double averageUserComments) {
+        this.averageUserComments = averageUserComments;
+    }
+
+    public void setAveragePostComments(double averagePostComments) {
+        this.averagePostComments = averagePostComments;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/TestForumCalculator.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/TestForumCalculator.java
@@ -1,11 +1,70 @@
 package com.kodilla.testing.forum.statistics;
 
+import com.kodilla.testing.forum.ForumComment;
+import com.kodilla.testing.forum.ForumPost;
+import com.kodilla.testing.forum.ForumUser;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.mockito.Mockito.*;
 
 public class TestForumCalculator {
 
+    @Mock
+    private Statistics statistics;
+
     @Test
-    void testcalculateAdvStatistics()
+    void testCalculateAdvStatisticsForZeroPosts()
+    {
+        //Given
+        ForumCalculator forumCalculator = new ForumCalculator(statistics);
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        List<ForumPost> list = new ArrayList<>();
+        when(statistics.postsCount()).thenReturn(list.size());
+
+        //When
+        int postNumber = forumCalculator.statistics.postsCount();
+
+        //Then
+        assertEquals(0,postNumber);
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForThousandPosts()
+    {
+        //Given
+        ForumCalculator forumCalculator = new ForumCalculator(statistics);
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        List<ForumPost> list = generateListOfPosts(1000);
+        when(statistics.postsCount()).thenReturn(list.size());
+
+        //When
+        int postNumber = forumCalculator.statistics.postsCount();
+
+        //Then
+        assertEquals(1000,postNumber);
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForZeroComments()
+    {
+        //Given
+        ForumCalculator forumCalculator = new ForumCalculator(statistics);
+        List<ForumComment> list = new ArrayList<>();
+        when(statistics.commentsCount()).thenReturn(list.size());
+        //When
+        int commentsNumber = forumCalculator.statistics.commentsCount();
+        //Then
+        assertEquals(0,commentsNumber);
+
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForLargerQuantityOfPostsThanComments()
     {
         //Given
 
@@ -13,5 +72,60 @@ public class TestForumCalculator {
 
         //Then
 
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForLargerQuantityOfCommentsThanPosts()
+    {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForZeroUsers()
+    {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    void testCalculateAdvStatisticsForHundredUsers()
+    {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    private List<ForumPost> generateListOfPosts(int numberOfPosts)
+    {
+        List<ForumPost> postList = new ArrayList<>();
+        for(int n = 0; n < numberOfPosts; n++)
+        {
+            ForumPost thePost = new ForumPost("Hello everyone, " + n, "mrsmith" + n);
+            postList.add(thePost);
+        }
+        return postList;
+    }
+
+    private List<ForumUser> generateListOfUsers(int numberOfUsers)
+    {
+        List<ForumUser> userList = new ArrayList<>();
+        for(int n = 0; n < numberOfUsers; n++)
+        {
+            ForumUser forumUser = new ForumUser("mrsmith" + n, "John Smith" + n);
+            userList.add(forumUser);
+        }
+        return userList;
     }
 }
